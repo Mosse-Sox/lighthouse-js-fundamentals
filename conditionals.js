5fc99ad4481d50d730267ac39d69bd3869e2a1ca
Mosse-Sox <autumngreenie990@gmail.com>
2023-05-12 13:02:31 +0000

    if else

--- conditionals.js
+++ conditionals.js
@@ -1,17 +1,7 @@
-const raining = false; 
 const cold = false; 
-const sunny = true; 
-
-if (raining) {
-  console.log('Don\'t forget your umbrella!'); 
-}
-
-if (sunny) {
-  console.log('You\'ll need sunscreen today!');
-}
 
 if (cold) {
-  console.log('Make sure you pick out a scarf!');
+  console.log("Make sure you pick out a scarf!");
+} else {
+  console.log("Short sleeves are fine.");
 }
-
-console.log('Now you\'re ready to go outside!'); 
