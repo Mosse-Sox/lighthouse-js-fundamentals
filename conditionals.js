780334e17d39a6cf4080d6c1511ac04c2b5a8935
Mosse-Sox <autumngreenie990@gmail.com>
2023-05-12 13:40:11 +0000

    adding more examples

--- conditionals.js
+++ conditionals.js
@@ -1,6 +1,17 @@
 const isCitizen = true;
 const age = 26; 
+const temperature = 55;
+const raining = false;
 
 if (isCitizen && age > 18) {
   console.log("You are eligible to vote.");
 }
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn\'t such a good idea...");
+}
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
+
