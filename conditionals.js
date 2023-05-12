cd57baba178bbc665059df7b062c323c33a3176a
Mosse-Sox <autumngreenie990@gmail.com>
2023-05-12 12:54:23 +0000

    testing conditial ifs

---
+++ conditionals.js
@@ -0,0 +1,17 @@
+const raining = false; 
+const cold = false; 
+const sunny = true; 
+
+if (raining) {
+  console.log('Don\'t forget your umbrella!'); 
+}
+
+if (sunny) {
+  console.log('You\'ll need sunscreen today!');
+}
+
+if (cold) {
+  console.log('Make sure you pick out a scarf!');
+}
+
+console.log('Now you\'re ready to go outside!'); 
